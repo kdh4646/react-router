2c813ccb2ab9b32bd97f9078a0f505aca2262ad9
Derek Kweon <kdh4646@hanmail.net>
2023-09-20 17:56:05 -0700

    2023-09-20 updating .css files

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -2,12 +2,15 @@
 import { Outlet } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
+import classes from "./Root.module.css";
 
 const RootLayout = () => {
   return (
     <>
       <MainNavigation />
-      <Outlet />
+      <main className={classes.content}>
+        <Outlet />
+      </main>
     </>
   );
 };
