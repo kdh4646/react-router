ab2c82aba91e571e1225a59b02bf5aa8f15f03a5
Derek Kweon <kdh4646@hanmail.net>
2023-09-20 18:10:47 -0700

    2023-09-20 update README, using isActive

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,17 +1,34 @@
-import { Link } from "react-router-dom";
+//NavLink: similar to "Link" but get className takes a function
+import { NavLink } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 
+//using "end" to make sure other link w/ common addresses are considered differently
 const MainNavigation = () => {
   return (
     <header className={classes.header}>
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
